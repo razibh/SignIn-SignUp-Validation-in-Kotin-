9c36ea97dd482d5a5ee21840f8a21f9976ccfeae
razibh <razib11@yahoo.com>
2023-05-30 23:47:31 +0600

    implement

---
+++ app/src/main/java/com/razib/signinsignup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.razib.signinsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
